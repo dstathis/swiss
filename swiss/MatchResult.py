ee37d62a7e3d8034b0d4569821e52fec6aa2eac9
Dylan Stephano-Shachter <dstephanoshachter@gmail.com>
2019-05-30 23:01:52 -0400

    match result class

---
+++ swiss/MatchResult.py
@@ -0,0 +1,29 @@
+WIN = 0
+LOSS = 1
+TIE = 2
+
+class InvalidResultError(Exception):
+    pass
+
+
+class MatchResult:
+    def __init__(self, main_result=None):
+        if not isinstance(main_result, int):
+            raise InvalidResultError()
+        self.main_result = main_result
+
+
+class MtgMatchResult(MatchResult):
+    def __init__(self, main_result=None, score=None):
+        super(MatchResult, self).__init__(main_result)
+        if (not isinstance(score, tuple)) or len(score) != 3:
+            raise InvalidResultError()
+        self.score = score
+
+
+class WhMatchResult(MatchResult):
+    def __init(self, main_result=None, score=None):
+        super(MatchResult, self).__init__(main_result)
+        if (not isinstance(score, tuple)) or len(score) != 2:
+            raise InvalidResultError()
+        self.score = score
